1237267163947c2343968286eb90325d07bc55dd
vedant241206 <vedantpawar434@gmail.com>
2025-10-02 14:22:23 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,17 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    if (username === '' || password === '') {
+        alert('Please fill in both fields.');
+        return;
+    }
+
+    
+    else { 
+        
+        window.location.href = 'index.html'; 
+    }
+});
