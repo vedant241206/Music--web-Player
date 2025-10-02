9fe594b5cbe50fc1acae175b6f57c09910c0d60d
vedant241206 <vedantpawar434@gmail.com>
2025-10-02 14:35:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ const playMusic = (track, pause = false) => {
 
 
 async function displayAlbums() {
-    let a = await fetch(`http://127.0.0.1:5500/songs/`);
+    let a = await fetch(`songs/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -98,7 +98,7 @@ async function displayAlbums() {
         if(e.href.includes("/songs/")){
             
             let folder= e.href.split("/").slice(-2)[1];
-            let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`);
+            let a = await fetch(`songs/${folder}/info.json`);
             let response = await a.json();  
             
             cardcontainer.innerHTML=cardcontainer.innerHTML+`<div data-folder="${folder}" class="card ">
